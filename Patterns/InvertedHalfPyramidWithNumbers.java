4b580b8544affc24f989d6550588595ade291ee5
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-06 03:00:01 +0530

    Another DSA Commit: 2024-07-06 03:00:01

--- Patterns/InvertedHalfPyramidWithNumbers.java
+++ Patterns/InvertedHalfPyramidWithNumbers.java
@@ -1,5 +1,14 @@
 package Patterns;
 
-public class InvertedHalfPyramidWithNumbers {
-    
+public class InvertedHalfPyramidWithNumbers { 
+    public static void main(String[] args) {
+        int num = 4;
+
+        for(int i = 1; i<= num; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
 }
