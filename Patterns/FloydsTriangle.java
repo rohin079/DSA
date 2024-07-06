cda40d8c02fe74dfed7199340443908909bc12ba
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-06 15:00:01 +0530

    Another DSA Commit: 2024-07-06 15:00:01

---
+++ Patterns/FloydsTriangle.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        int num = 5;
+        int counter = 1;
+        for(int i=1; i<=num; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println(" ");
+        }
+   
+    }
+    
+}
--- Patterns/InvertedHalfPyramidWithNumbers.java
+++ Patterns/InvertedHalfPyramidWithNumbers.java
@@ -2,10 +2,10 @@ package Patterns;
 
 public class InvertedHalfPyramidWithNumbers { 
     public static void main(String[] args) {
-        int num = 4;
+        int num = 5;
 
         for(int i = 1; i<= num; i++){
-            for (int j=1; j<=i; j++){
+            for (int j=1; j<=num-i+1; j++){
                 System.out.print(j);
             }
             System.out.println(" ");
