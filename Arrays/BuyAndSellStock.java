ad3fbefb3f84665129c7cdfd47e20993538306e1
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-07 00:00:01 +0530

    Another DSA Commit: 2024-07-07 00:00:01

--- Arrays/BuyAndSellStock.java
+++ Arrays/BuyAndSellStock.java
@@ -1,5 +1,31 @@
 package Arrays;
 
 public class BuyAndSellStock {
-    
+    public static int timeToBuyStock(int prices[]) {
+
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+
+            if (buyPrice > prices[i]) {
+                buyPrice = prices[i];
+            }
+        }
+
+        return maxProfit;
+
+    };
+
+    public static void main(String[] args) {
+
+        int prices[] = { 2, 5, 7, 0, 3, 8, 3 };
+
+        System.out.println(timeToBuyStock(prices));
+
+    }
 }
