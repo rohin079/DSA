e2caf141fc5c247bc235bcfd6ba8a4e4ff9e5444
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-07 21:00:01 +0530

    Another DSA Commit: 2024-07-07 21:00:01

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,18 +1,24 @@
 package Sorting;
 
+//insertion sort works by inserting the current element from unsorted part in thwir correct position in the sorted part. Shifts the number greater than the current number to right
+
+//Best video: https://www.youtube.com/watch?v=0lOnnd50cGI
+
+//time complexity = O(n^2)
+
 public class InsertionSort {
     public static void sortArray(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            int minPos = i;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minPos]) {
-                    minPos = j;
-                }
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i-1;
 
-                int temp = arr[minPos];
-                arr[minPos] = arr[i];
-                arr[i] = temp;
+            //finding out the current position to insert the element from the unsorted array
+            while( prev>=0 && arr[prev]> curr){
+                arr[prev+1] = arr[prev];
+                prev--;
             }
+            //insertion
+            arr[prev+1] = curr;
         }
     }
 
