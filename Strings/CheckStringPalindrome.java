8546493b5d29f31bbc455386b5dd857e09a95ae5
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-08 14:00:01 +0530

    Another DSA Commit: 2024-07-08 14:00:01

--- Strings/CheckStringPalindrome.java
+++ Strings/CheckStringPalindrome.java
@@ -1,16 +1,21 @@
 package Strings;
 
-public class CheckStringPalindrome{
+public class CheckStringPalindrome {
 
-    public static boolean isPalindrome(String str){
-        
-        for(int i=0; i<(str.length()/2); i++){
-            
+    public static boolean isPalindrome(String str) {
+
+        for (int i = 0; i < (str.length() / 2); i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                return false;
+            }
         }
-        return false;
+        return true;
     }
 
     public static void main(String[] args) {
-        
+        String str = "racecar";
+
+        System.out.println(isPalindrome(str));
+
     }
 }
