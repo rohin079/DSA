eab0f1eb2f55846d018b6498c05289730cbe294a
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-12 22:00:01 +0530

    Another DSA Commit: 2024-07-12 22:00:01

--- Linked_Lists/LinkedList.java
+++ Linked_Lists/LinkedList.java
@@ -29,12 +29,41 @@ public class LinkedList {
     }
 
     public void addLast(int data){
-        Node new
+        Node newNode = new Node(data);
+        
+        if (Head == null) {
+            Head = Tail = newNode;
+            return;
+        }
+
+        Tail.next = newNode;
+        Tail = newNode;
+        newNode.next = null;
+    }
+
+    public void PrintLinkedList(){
+        Node temp = Head;
+
+        if(Head == null){
+            System.out.println("Linked list is empty");
+        }
+
+       while(temp != null){
+        System.out.println(temp.data);
+        temp = temp.next;
+       }
+
     }
 
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
+
+        ll.addFirst(1);
+        ll.addFirst(4);
+        ll.addLast(5);;
+
+        ll.PrintLinkedList();
 
     }
 }
