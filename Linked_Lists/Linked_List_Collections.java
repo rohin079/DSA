3606f6c15690b853e223d18b9d5ca34371627f1e
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-14 18:00:01 +0530

    Another DSA Commit: 2024-07-14 18:00:01

---
+++ Linked_Lists/Linked_List_Collections.java
@@ -0,0 +1,18 @@
+package Linked_Lists;
+
+import java.util.LinkedList;
+
+public class Linked_List_Collections {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> ll = new LinkedList<>();
+
+        ll.addLast(2);
+        ll.addLast(5);
+        ll.addFirst(1);
+
+        System.out.println(ll);
+        
+    }
+    
+}
