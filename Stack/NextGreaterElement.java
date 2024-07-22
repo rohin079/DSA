69d92407e6c394ad64ae64e2e3564ff3b6be0083
Rohin Mehrotra <38971621+rohin079@users.noreply.github.com>
2024-07-22 19:00:01 +0530

    Another DSA Commit: 2024-07-22 19:00:01

--- Stack/NextGreaterElement.java
+++ Stack/NextGreaterElement.java
@@ -1,10 +1,33 @@
 package Stack;
 
 import java.util.*;
+
 public class NextGreaterElement {
 
-
     public static void main(String[] args) {
-        Stack<Stack> 
+
+        int arr[] = {6, 8, 0, 1, 3};
+
+        Stack<Integer> s = new Stack<>();
+        int nextGreaterElement[] = new int[arr.length];
+
+        for(int i = arr.length -1; i>=0; i--){
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                nextGreaterElement[i] = -1;
+            }
+
+            else{
+                nextGreaterElement[i] = s.peek();
+        }
+
+            s.push(i);
+
     }
+
+    for(int i=0; )
+}
 }
